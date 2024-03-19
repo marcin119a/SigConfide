219c9c83a1c176e075ab225c474a03eb8e01dab5
Marcin Wierzbiński <marcin119a@gmail.com>
2024-03-19 13:21:26 +0100

    drop zeros columns

--- sigconfide/modelselection/analyzer.py
+++ sigconfide/modelselection/analyzer.py
@@ -4,7 +4,7 @@ from sigconfide.utils import utils
 import numpy as np
 import os
 import sys
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 module_path = os.path.dirname(utils.__file__)
 
@@ -27,7 +27,9 @@ def process_sample(args):
         print(f"Error processing sample {i}: {e}")
         return (i, None, None)
 
-def cosmic_fit(samples_file, output_file, threshold=0.01, mutation_count=None, R=100, significance_level=0.01, cosmic_version=3.4):
+def cosmic_fit(samples_file, output_file, threshold=0.01,
+               mutation_count=None, R=100, significance_level=0.01, cosmic_version=3.4,
+               drop_zeros_columns=False):
     COSMIC, names_signatures = load_signatures_file(versions[cosmic_version])
     samples, names_patients = load_samples_file(samples_file)
 
@@ -36,7 +38,7 @@ def cosmic_fit(samples_file, output_file, threshold=0.01, mutation_count=None, R
 
     args_list = [(i, samples[:, i], COSMIC, threshold, mutation_count, R, significance_level) for i in range(samples.shape[1])]
 
-    with ProcessPoolExecutor() as executor:
+    with ThreadPoolExecutor() as executor:
         for i, best_columns, estimation_exposures in executor.map(process_sample, args_list):
             if best_columns is not None:
                 for ind, col in enumerate(best_columns):
@@ -47,6 +49,12 @@ def cosmic_fit(samples_file, output_file, threshold=0.01, mutation_count=None, R
                 sys.stdout.write('\r')
                 sys.stdout.write("[%-20s] %d%%" % ('=' * int(20 * percent), 100 * percent))
                 sys.stdout.flush()
-
-    np.savetxt(output_file + "/Assignment_Solution_Activities.csv", output, delimiter=",", fmt='%s')
-
+    if not drop_zeros_columns:
+        np.savetxt(output_file + "/Assignment_Solution_Activities.csv", output, delimiter=",", fmt='%s')
+    else:
+        is_non_zero_column = np.array([i != 0 if i > 0 else True for i in range(output.shape[1])])
+        for col in range(1, output.shape[1]):
+            if np.all(output[1:, col].astype(np.float32) == 0.0):
+                is_non_zero_column[col] = False
+        filtered_output = output[:, is_non_zero_column]
+        np.savetxt(output_file + "/Assignment_Solution_Activities.csv", filtered_output, delimiter=',', fmt='%s')
